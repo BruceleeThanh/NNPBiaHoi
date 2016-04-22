d428a7f17c13b8cb98d5d86604e87f8b39a622f5
kienbui1995 <kien.laohac@gmail.com>
2016-04-23 00:18:39 +0700

    Uc phương tiện: Form thêm, sửa phương tiện

---
+++ NPPBiaHoi/ucPhuongTien/frmSuaPhuongTien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace NPPBiaHoi.ucPhuongTien
+{
+    public partial class frmSuaPhuongTien : DevExpress.XtraEditors.XtraForm
+    {
+        public frmSuaPhuongTien()
+        {
+            InitializeComponent();
+        }
+    }
+}
