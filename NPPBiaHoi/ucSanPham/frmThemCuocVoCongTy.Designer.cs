3c566387c32656a41126f6c11b007fd3635d0c7b
kienbui1995 <kien.laohac@gmail.com>
2016-04-25 19:23:33 +0700

    Thêm cược vỏ công ty

---
+++ NPPBiaHoi/ucSanPham/frmThemCuocVoCongTy.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace NPPBiaHoi.ucSanPham
+{
+    partial class frmThemCuocVoCongTy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmThemCuocVoCongTy));
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.lueSanPham = new DevExpress.XtraEditors.LookUpEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.timeThoiGian = new DevExpress.XtraEditors.DateEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.spinSoLuong = new DevExpress.XtraEditors.SpinEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.txtDonGia = new DevExpress.XtraEditors.TextEdit();
+            this.btnLuu = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.lueSanPham.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeThoiGian.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeThoiGian.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinSoLuong.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDonGia.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.labelControl1.Location = new System.Drawing.Point(13, 70);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(69, 17);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Sản phẩm";
+            // 
+            // lueSanPham
+            // 
+            this.lueSanPham.Location = new System.Drawing.Point(88, 67);
+            this.lueSanPham.Name = "lueSanPham";
+            this.lueSanPham.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lueSanPham.Size = new System.Drawing.Size(153, 22);
+            this.lueSanPham.TabIndex = 1;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.labelControl2.Location = new System.Drawing.Point(13, 36);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(63, 17);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "Thời gian";
+            // 
+            // timeThoiGian
+            // 
+            this.timeThoiGian.EditValue = new System.DateTime(2016, 4, 25, 0, 0, 0, 0);
+            this.timeThoiGian.Location = new System.Drawing.Point(88, 36);
+            this.timeThoiGian.Name = "timeThoiGian";
+            this.timeThoiGian.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeThoiGian.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeThoiGian.Properties.DisplayFormat.FormatString = "";
+            this.timeThoiGian.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            this.timeThoiGian.Properties.EditFormat.FormatString = "";
+            this.timeThoiGian.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            this.timeThoiGian.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Buffered;
+            this.timeThoiGian.Properties.Mask.EditMask = "T";
+            this.timeThoiGian.Size = new System.Drawing.Size(153, 22);
+            this.timeThoiGian.TabIndex = 3;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.labelControl3.Location = new System.Drawing.Point(13, 103);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(62, 17);
+            this.labelControl3.TabIndex = 4;
+            this.labelControl3.Text = "Số lượng";
+            // 
+            // spinSoLuong
+            // 
+            this.spinSoLuong.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.spinSoLuong.Location = new System.Drawing.Point(88, 99);
+            this.spinSoLuong.Name = "spinSoLuong";
+            this.spinSoLuong.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.spinSoLuong.Size = new System.Drawing.Size(153, 22);
+            this.spinSoLuong.TabIndex = 5;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.labelControl4.Location = new System.Drawing.Point(13, 157);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(49, 16);
+            this.labelControl4.TabIndex = 6;
+            this.labelControl4.Text = "Đơn giá";
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(88, 151);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(153, 22);
+            this.txtDonGia.TabIndex = 7;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.btnLuu.Appearance.Options.UseFont = true;
+            this.btnLuu.Image = ((System.Drawing.Image)(resources.GetObject("btnLuu.Image")));
+            this.btnLuu.Location = new System.Drawing.Point(73, 197);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 32);
+            this.btnLuu.TabIndex = 8;
+            this.btnLuu.Text = "Lưu";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold);
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(166, 197);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 32);
+            this.simpleButton1.TabIndex = 9;
+            this.simpleButton1.Text = "Hủy";
+            // 
+            // frmThemCuocVoCongTy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 241);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.txtDonGia);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.spinSoLuong);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.lueSanPham);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.timeThoiGian);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmThemCuocVoCongTy";
+            this.Text = "Thêm cược vỏ công ty";
+            ((System.ComponentModel.ISupportInitialize)(this.lueSanPham.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeThoiGian.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeThoiGian.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinSoLuong.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDonGia.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LookUpEdit lueSanPham;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.DateEdit timeThoiGian;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SpinEdit spinSoLuong;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.TextEdit txtDonGia;
+        private DevExpress.XtraEditors.SimpleButton btnLuu;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
---
+++ NPPBiaHoi/ucSanPham/frmThemCuocVoCongTy.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace NPPBiaHoi.ucSanPham
+{
+    public partial class frmThemCuocVoCongTy : DevExpress.XtraEditors.XtraForm
+    {
+        public frmThemCuocVoCongTy()
+        {
+            InitializeComponent();
+        }
+    }
+}
