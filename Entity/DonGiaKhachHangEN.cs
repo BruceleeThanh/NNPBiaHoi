1e8575110cc2f77d5beaa5df35e2027f8606feca
kienbui1995 <kien.laohac@gmail.com>
2016-05-11 17:16:11 +0700

    bổ sung Entity Đơn Giá khách hàng

---
+++ Entity/DonGiaKhachHangEN.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess;
+using BussinessLogic;
+
+namespace Entity
+{
+     public class DonGiaKhachHangEN: DonGiaKhachHang
+    {
+        private KhachHangBO aKhachHangBO = new KhachHangBO();
+        private SanPhamBO aSanPhamBO = new SanPhamBO();
+        public string TenKhachHang { get; set; }
+        public string TenSanPham { get; set; }
+
+        public DonGiaKhachHangEN(DonGiaKhachHang aDonGiaKhachHang)
+        {
+            this.Ma = aDonGiaKhachHang.Ma;
+            this.MaKhachHang = aDonGiaKhachHang.MaKhachHang;
+            this.MaSanPham = aDonGiaKhachHang.MaSanPham;
+            this.DonGia = aDonGiaKhachHang.DonGia;
+            this.ThoiGianCapNhat = aDonGiaKhachHang.ThoiGianCapNhat;
+            TenKhachHang = aKhachHangBO.Select_ByMa(aDonGiaKhachHang.MaKhachHang).Ten;
+            TenSanPham = aSanPhamBO.Select_ByMa(aDonGiaKhachHang.MaSanPham).Ten;
+        }
+
+    }
+}
